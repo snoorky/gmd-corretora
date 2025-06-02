39e1aea4f4f7cdd7c975c127fbf75e6e130bfa78
Raphael Vitor <raphaelvitorlopes@icloud.com>
2025-06-02 08:09:03 -0300

    fix: adjust images size

--- src/app/_components/insurances.tsx
+++ src/app/_components/insurances.tsx
@@ -16,8 +16,7 @@ const insurances = [
     title: "Seguro Auto",
     description: "Proteção completa para seu veículo com assistência 24h",
     buttonLabel: "Proteger meu Veículo",
-    buttonText:
-      "Olá!+Quero+saber+mais+sobre+o+Seguro+Auto+com+proteção+completa+e+assistência+24h",
+    buttonText: "Olá!+Quero+saber+mais+sobre+o+Seguro+Auto+com+proteção+completa+e+assistência+24h",
   },
   {
     image: "seguro-residencial",
@@ -38,8 +37,7 @@ const insurances = [
   {
     image: "seguro-viagem",
     title: "Seguro Viagem",
-    description:
-      "Proteção completa para você viajar tranquilo, no Brasil ou exterior",
+    description: "Proteção completa para você viajar tranquilo, no Brasil ou exterior",
     buttonLabel: "Viajar com Segurança",
     buttonText:
       "Olá!+Estou+planejando+uma+viagem+e+gostaria+de+contratar+um+Seguro+Viagem.+Pode+me+ajudar?",
@@ -49,8 +47,7 @@ const insurances = [
     title: "Seguro Garantia",
     description: "Garantias para contratos, licitações e processos judiciais",
     buttonLabel: "Solicitar uma Garantia",
-    buttonText:
-      "Olá!+Preciso+de+informações+sobre+Seguro+Garantia+para+contratos+ou+licitações.",
+    buttonText: "Olá!+Preciso+de+informações+sobre+Seguro+Garantia+para+contratos+ou+licitações.",
   },
   {
     image: "seguro-fianca",
@@ -65,8 +62,7 @@ const insurances = [
     title: "Responsabilidade Civil",
     description: "Coberturas para RC Geral, Profissional, Médico, D&O e outros",
     buttonLabel: "Solicitar Cotação de RC",
-    buttonText:
-      "Olá!+Tenho+interesse+em+um+seguro+de+Responsabilidade+Civil.+Pode+me+orientar?",
+    buttonText: "Olá!+Tenho+interesse+em+um+seguro+de+Responsabilidade+Civil.+Pode+me+orientar?",
   },
 ];
 
@@ -83,11 +79,11 @@ export default function Insurances() {
               className="flex flex-col rounded-2xl justify-between overflow-hidden pb-2 md:pb-4 border border-orange bg-white"
             >
               <div className="">
-                <div className="relative w-[170px] md:w-[340px] lg:w-[410px] xl:w-[280px] h-[115px] md:h-[230px] lg:h-[276px] xl:h-[190px]">
+                <div className="relative w-[170px] md:w-[340px] lg:w-[410px] xl:w-[280px] 2xl:w-[400px] h-[115px] md:h-[230px] lg:h-[276px] xl:h-[190px] 2xl:h-[266px]">
                   <Image
                     alt={item.title}
                     src={`/${item.image}.png`}
-                    sizes="(max-width: 767px) 170px, (max-width: 1023px) 340px, (max-width: 1279px) 410px, 280px"
+                    sizes="(max-width: 767px) 170px, (max-width: 1023px) 340px, (max-width: 1279px) 410px, 500px"
                     className="absolute object-cover"
                     loading="lazy"
                     fill
@@ -119,9 +115,7 @@ export default function Insurances() {
             fill
           />
           <div className="relative text-left space-y-2 xl:space-y-6 w-[72%] md:w-[50%] xl:w-[30%]">
-            <h3>
-              Fale com a gente e nós encontraremos o seguro ideal para você!
-            </h3>
+            <h3>Fale com a gente e nós encontraremos o seguro ideal para você!</h3>
             <Forms label="Seguros" />
           </div>
         </div>
